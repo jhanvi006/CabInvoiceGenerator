b6d220982e1ca722ec9c0e0bdb6b8016e3fba07d
jhanvi006 <jhanvikankhara06@gmail.com>
2022-05-28 02:34:01 +0530

    Added: test case for minimum fare added.

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -2,9 +2,12 @@ package com.bridgelabz;
 
 public class InvoiceGenerator {
     private static final double MIN_COST_PER_KM = 10.0;
+    private static final double MIN_FARE = 5;
     public final double COST_PER_TIME = 1.0;
     public double calculateFare(double distance, int time) {
-        double fare = distance * MIN_COST_PER_KM + time * COST_PER_TIME;
-        return fare;
+        double totalFare = distance * MIN_COST_PER_KM + time * COST_PER_TIME;
+        if (totalFare < MIN_FARE)
+            return MIN_FARE;
+        return totalFare;
     }
 }
