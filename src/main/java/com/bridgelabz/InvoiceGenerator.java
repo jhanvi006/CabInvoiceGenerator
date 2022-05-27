3ae5abd86731e6e73c825602ecadbfd76e5628e3
jhanvi006 <jhanvikankhara06@gmail.com>
2022-05-28 02:58:19 +0530

    Added: calculation of fare and test for multiple rides is added

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -6,8 +6,14 @@ public class InvoiceGenerator {
     public final double COST_PER_TIME = 1.0;
     public double calculateFare(double distance, int time) {
         double totalFare = distance * MIN_COST_PER_KM + time * COST_PER_TIME;
-        if (totalFare < MIN_FARE)
-            return MIN_FARE;
+        return Math.max(totalFare, MIN_FARE);
+    }
+
+    public double calculateFare(Ride[] rides) {
+        double totalFare = 0.0;
+        for (Ride ride : rides) {
+            totalFare += this.calculateFare(ride.distance, ride.time);
+        }
         return totalFare;
     }
 }
--- src/test/java/com/bridgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -12,7 +12,6 @@ public class InvoiceServiceTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assertions.assertEquals(35, fare);
     }
-
     @Test
     public void givenLessDistanceAndTime_ShouldReturnMinimumFare(){
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
@@ -20,5 +19,13 @@ public class InvoiceServiceTest {
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assertions.assertEquals(5, fare);
+    }
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare(){
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = { new Ride(3.0, 5),
+                new Ride(0.1, 1) };
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assertions.assertEquals(40, fare, 0.0);
     }
 }
